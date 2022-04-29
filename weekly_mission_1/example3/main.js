74deb5d79b88a59761102940311c17f68b429936
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-28 22:51:29 -0500

    Update main.js
    Update en main js, se le ponen los valores a los logger

---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,5 @@
+const logger1 = require('./logger_1')
+const logger2 = require('./logger_2')
+
+logger1('Prueba1')
+logger2.verbose('Prueba verbose')//Aquí nos tenemos que meter al objeto específico
