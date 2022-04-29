d9d55aa3d224e89b69ebae7f0ab45cf846f00181
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-28 22:12:36 -0500

    Cambios
    Cambios en mainjs

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -1,6 +1,30 @@
-let myCar = new Object();
+//Creación de un objeto
+let myCar = new Object(); 
+
+//Atributos del objeto que creamos
 myCar.make = 'Ford';
 myCar.model = 'Mustang';
 myCar.year = 1950;
 
+//Impresión del objeto
 console.log(myCar)
+
+//Declaración de un objeto con varuables locales y públicas
+const myModule = (() => {
+
+    //Varibale de contexto local
+    const privateFoo = "Hola, soy privado, sólo me usan dentro de este objeto"
+    const privateBar = [1,2,3,4,5]
+    const baz = "Soy un valor que va a ser expuesto"
+
+    //Variable para guardar las variables locales
+    const exported = {
+        publicFoo: "Valor público, pueden verme desde donde me llamen",
+        publicBar: "Otro valor público",
+        publicBaz: baz
+    }
+
+    return exported
+})()
+
+console.log(myModule)
