565b10de12b8087545052b757978791f261d90cf
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-28 22:37:41 -0500

    Corrección
    Correcciones necesarias, equivocación de archivo

--- weekly_mission_1/example2/mains.js
+++
@@ -1,8 +0,0 @@
-//Se crea la variable y se exporta para que otra clase la pueda usar
-exports.info = (message) => {
-    console.log(`info: ${ message}`)
-}
-
-exports.verbose = (message) => {
-    console.log(`info: ${message}`)
-}
