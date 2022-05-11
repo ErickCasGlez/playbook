2e45615fd557fa3b08e73a50b2227a903c1d65d2
ErickCasGlez <erick.cas.glez@gmail.com>
2022-05-10 21:17:49 -0500

    Create different.js
    Se crea un objeto con diferentes propiedades y se manda a imprimir, se van pidiendo propiedades específicas

---
+++ weekly_mission_2/Objetos/different.js
@@ -0,0 +1,18 @@
+const Dif = {
+    name: "Erick",
+    age: 22,
+    gustos: [
+        "Comer",
+        "Dormir",
+        "Música"
+    ],
+    musica: {
+        reggeaton: "Bad Bunny",
+        pop: "Jeese Baez",
+        elextronica: "Skrillex"
+    }
+}
+
+console.log(Dif);
+console.log(Dif.gustos);
+console.log(Dif["musica"]);
